1d44beeff83aadaec451c5ef761aa62b44f5cc75
DEEPAK GADWAL <deepak.gadwal008@@gmail.com>
2024-07-25 09:30:00 +0530

    handle pagniation error

--- src/App.jsx
+++ src/App.jsx
@@ -13,9 +13,6 @@ const TableComponent = () => {
     const fetchData = async () => {
       try {
         const response = await fetch(ENDPOINT);
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
         const result = await response.json();
         setData(result);
       } catch (error) {
